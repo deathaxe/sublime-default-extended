dd7cb6cf01348c09670d4147bed626fef5605f61
deathaxe <deathaxe82@googlemail.com>
2024-04-28 18:31:16 +0200

    Add quick_panel_pageup / quick_panel_pagedown commands
    Add support for navigating in quick panels via pageup/pagedown keys.

---
+++ quick_panel.py
@@ -0,0 +1,16 @@
+import sublime
+import sublime_plugin
+
+
+class QuickPanelPageUpCommand(sublime_plugin.WindowCommand):
+    """Simulate page-up by repeating up command multiple times"""
+    def run(self, count=8):
+        for i in range(count):
+            self.window.run_command("move", {"by": "lines", "forward": False})
+
+
+class QuickPanelPageDownCommand(sublime_plugin.WindowCommand):
+    """Simulate page-down by repeating down command multiple times"""
+    def run(self, count=8):
+        for i in range(count):
+            self.window.run_command("move", {"by": "lines", "forward": True})
